2f3f57ff79c0492f3ad323ad64a3503249f78114
LiHui_QIAN <1157577993@qq.com>
2024-11-18 15:58:25 +0800

    docs：调整目录结构

--- Example/QtDemo/main.cpp
+++ Example/QtDemo/main.cpp
@@ -1,10 +1,42 @@
-﻿#include "mainwindow.h"
-#include <QApplication>
+﻿#include <QApplication>
+#include <QPushButton>
+#include <QEvent>
+#include <QMouseEvent>
+#include <QResizeEvent>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+class ButtonEventFilter : public QObject {
+public:
+    ButtonEventFilter(QPushButton* button, QObject* parent = nullptr)
+        : QObject(parent), m_button(button), m_originalSize(button->size()) {}
+
+protected:
+    bool eventFilter(QObject* obj, QEvent* event) override {
+        if (event->type() == QEvent::Enter) {
+            m_button->resize(m_originalSize * 1.2); // 放大按钮
+        }
+        else if (event->type() == QEvent::Leave) {
+            m_button->resize(m_originalSize); // 恢复按钮大小
+        }
+        return QObject::eventFilter(obj, event);
+    }
+
+    void setButton(QPushButton* button) {
+        m_button = button;
+        m_originalSize = button->size();
+    }
+
+private:
+    QPushButton* m_button;
+    QSize m_originalSize;
+};
+
+int main(int argc, char* argv[]) {
+    QApplication app(argc, argv);
+
+    QPushButton button("Hover over me!");
+    button.resize(200, 200); // 设置按钮的初始大小
+
+    button.show();
+
+    return app.exec();
 }
